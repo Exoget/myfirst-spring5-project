9eddc03d9f4af944980b2ebe25e8f6abb1e01a54
fferchichi <ferchichi.feres@gmail.com>
2020-11-28 19:51:00 +0100

    first commit

---
+++ src/main/java/com/packt/spring5webapp/MyfirstSpring5ProjectApplication.java
@@ -0,0 +1,13 @@
+package com.packt.spring5webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyfirstSpring5ProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyfirstSpring5ProjectApplication.class, args);
+	}
+
+}
