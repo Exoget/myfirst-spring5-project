84c0ec6629bca6d869e0581df7e289b394dc73db
fferchichi <ferchichi.feres@gmail.com>
2020-12-05 18:31:45 +0100

    add comment

--- src/main/java/com/packt/spring5webapp/repositories/PublisherRepository.java
+++ src/main/java/com/packt/spring5webapp/repositories/PublisherRepository.java
@@ -5,4 +5,6 @@ import org.springframework.data.repository.CrudRepository;
 import com.packt.spring5webapp.domain.Publisher;
 
 public interface PublisherRepository extends CrudRepository<Publisher, Long> {
+	// si j'aoute une mdethode ici ca va poser des problemes car on va utiliser l'implimentation par defaut
+	// SimpleJpaRepository qui ne contient pas la methode qui sera ajouté nromalement
 }
